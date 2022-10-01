c5a8485aaba2b47096414d0e48667cf0d554b4a0
Êxodo Gurgel <exodowellis@gmail.com>
2022-10-01 09:55:36 -0300

    finished styles Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,4 +1,4 @@
-import { SuccessContainer } from './styles'
+import { Border, SuccessContainer } from './styles'
 
 import illustrationImage from '../../assets/illustration.png'
 import { InfoWithIcon } from '../../components/InfoWithIcon'
@@ -10,31 +10,33 @@ export function Success() {
       <div>
         <h1>uhu! Pedido confirmado</h1>
         <p>Agora é só aguardar que logo o café chegará até você</p>
-        <div>
-          <InfoWithIcon
-            text="Entrega em "
-            address="Rua João Daniel Martinelli, 101"
-            subtitle="Farrapos - Porto Alegre, RS"
-            bgIcon="purple"
-            icon={<MapPin />}
-          />
+        <Border>
+          <div>
+            <InfoWithIcon
+              text="Entrega em "
+              address="Rua João Daniel Martinelli, 101"
+              subtitle="Farrapos - Porto Alegre, RS"
+              bgIcon="purple"
+              icon={<MapPin />}
+            />
 
-          <InfoWithIcon
-            text="Previsão de entrega"
-            subtitle="20 min - 30 min"
-            bgIcon="yellow"
-            icon={<Timer />}
-            textBold
-          />
+            <InfoWithIcon
+              text="Previsão de entrega"
+              subtitle="20 min - 30 min"
+              bgIcon="yellow"
+              icon={<Timer />}
+              textBold
+            />
 
-          <InfoWithIcon
-            text="Pagamento na entrada"
-            subtitle="Cartão de Crédito"
-            bgIcon="yellowDark"
-            icon={<CurrencyDollar />}
-            textBold
-          />
-        </div>
+            <InfoWithIcon
+              text="Pagamento na entrada"
+              subtitle="Cartão de Crédito"
+              bgIcon="yellowDark"
+              icon={<CurrencyDollar />}
+              textBold
+            />
+          </div>
+        </Border>
       </div>
       <div>
         <img src={illustrationImage} alt="" />
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,5 +1,51 @@
 import styled from 'styled-components'
 
 export const SuccessContainer = styled.div`
-  margin-block: 9rem;
+  margin-top: 11.5rem;
+  margin-bottom: 2rem;
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 800;
+    line-height: 1.3;
+
+    color: ${(props) => props.theme['yellow-700']};
+  }
+
+  p {
+    font-size: 1.25rem;
+    line-height: 1.3;
+
+    color: ${(props) => props.theme['gray-800']};
+  }
+`
+
+export const Border = styled.div`
+  margin-top: 2.5rem;
+  background: linear-gradient(102.89deg, #dbac2c 2.61%, #8047f8 98.76%);
+  max-width: 32.875rem;
+  height: 16.875rem;
+  border-radius: 6px 36px;
+
+  position: relative;
+
+  > div {
+    background-color: ${(props) => props.theme['gray-100']};
+    position: absolute;
+    padding: 2.5rem;
+
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    top: 1px;
+    left: 1px;
+    right: 1px;
+    bottom: 1px;
+    border-radius: 6px 36px;
+  }
 `
